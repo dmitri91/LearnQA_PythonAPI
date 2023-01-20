0518534f1f0250fdc22c0159bebe865303c4c41b
dalariono1 <dalariono1@example.com>
2023-01-20 07:36:55 +0300

    upd task

--- lesson_3/method_header.py
+++ lesson_3/method_header.py
@@ -3,6 +3,6 @@ import requests
 
 def test_check_header():
     response = requests.get("https://playground.learnqa.ru/api/homework_header")
-    print(response.text)
-    assert 'success' in response.text, "There is not header in the response"
-    assert '!' == (response.json())['success'], 'There is incorrect value headers in the response'
+    print(response.headers)
+    assert 'x-secret-homework-header' in response.headers, "There is not header in the response"
+    assert 'Some secret value' == response.headers['x-secret-homework-header'], 'There is incorrect value headers in the response'
